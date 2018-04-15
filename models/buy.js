8b03e8e5ce12b4e6c1c3660ce55e8f3afcf78fb8
chaudharyS <srishti_chaudhary1@yahoo.in>
2018-04-15 22:11:33 +0530

    Add files via upload

---
+++ models/buy.js
@@ -0,0 +1,12 @@
+var mongoose = require("mongoose");
+
+var buySchema = new mongoose.Schema({
+   username : {type: String, unique: true},
+   buys : [{
+               policy : String,
+               time : Number,
+               sum : Number
+   }]
+});
+
+module.exports = mongoose.model("Buy", buySchema);
